7cba3659769f18313a36e31ca083279c88530608
코린이 <113786196+kogunwoo@users.noreply.github.com>
2024-07-18 22:56:05 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length + 1];
+        
+        if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
+            System.arraycopy(num_list, 0, answer, 0, num_list.length);
+            answer[num_list.length] = num_list[num_list.length - 1] - num_list[num_list.length - 2];
+        } else {
+            System.arraycopy(num_list, 0, answer, 0, num_list.length);
+            answer[num_list.length] = num_list[num_list.length - 1] * 2;
+        }
+        return answer;
+    }
+}
+//System.arraycopy 는 바자에서 배열간의 복사를 수행하는 메서드 
+//System.arraycopy(원본 배열, 원본배열의 시작인덱스. 대상배열, 대상배열의 시작인덱스,복사할 길이)
+// 원본배열: 복사하고자 하는 원본 배열
+// 원본배열의 시작인덱스: 복사를 시작할 원본 배열의 인덱스
+// 대상배열: 복사된 원본 배열의 요소들이 저장될 대상 배열
+// 대상배열의 시작인덱스: 복사된 요소들이 대상 배열에서 저장될 시작 인덱스
+// 복사할 길이: 복사할 요소들의 개수
