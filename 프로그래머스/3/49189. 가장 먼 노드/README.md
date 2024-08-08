313a9bbde473b4090f8e7b8689a1346c369849ec
코린이 <113786196+kogunwoo@users.noreply.github.com>
2024-08-08 21:46:35 +0900

    [level 3] Title: 가장 먼 노드, Time: 40.29 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189) 
 
 ### 성능 요약
 
-메모리: 110 MB, 시간: 64.45 ms
+메모리: 107 MB, 시간: 40.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 08일 21:07:28
+2024년 08월 08일 21:46:29
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[][] edge) {
-      Map<Integer, List<Integer>> graph = new HashMap<>(); 
+     Map<Integer, List<Integer>> graph = new HashMap<>(); 
         for(int i =1; i<=n; i++){
             graph.put(i, new ArrayList<>());
         }
@@ -10,27 +10,27 @@ class Solution {
             graph.get(e[1]).add(e[0]);
         }
         
-        boolean[] visited = new boolean[n+1];
-        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[] visited  = new boolean[n+1];
+        Queue<int []> queue = new ArrayDeque<>();
         queue.add(new int[]{1,0});
-        visited[1] = true;
+        visited[1] =true;
         
-        int maxDist=0,  count =0;
+        int maxDist = 0, count = 0;
         while(!queue.isEmpty()){
             int[] cur = queue.remove();
             
-            if(maxDist < cur[1]){
-                maxDist = cur[1];
-                count = 1;
-            }else if(maxDist == cur[1]){
-                count++;
-            }
+        if(maxDist < cur[1]){
+            maxDist = cur[1];
+            count=1;
+        }else if(maxDist == cur[1]){
+            count ++;
+        }
             for(int next : graph.get(cur[0])){
                 if(visited[next]) continue;
-                    visited[next] =true;
-                queue.add(new int[]{next, cur[1]+1});
+                    visited[next] = true;
+                    queue.add(new int[]{next, cur[1]+1});
+                }
             }
+            return count;
         }
-        return count;
-    }
 }
